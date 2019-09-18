6217a35f5c6101f208d7f569f4ce73c50b195cef
Patrick Cook <pcookcollege@gmail.com>
2019-09-18 14:36:18 -0700

    First view

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module Spotifyme
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    # RSpotify::authenticate(ENV["CLIENT_ID"], ENV["CLIENT_SECRET"])
   end
 end
