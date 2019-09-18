eaede5b07e3c00a354e564b6974c6ebdaae009f0
Patrick Cook <pcookcollege@gmail.com>
2019-09-18 16:28:38 -0700

    Added SpotifyMe page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,30 @@
+require 'json'
+
 class UserController < ApplicationController
-  def spotify
+  @@user_hash = nil
+
+  def spotify_callback
     @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-    
+    @@user_hash = @spotify_user.to_hash
+  end
+
+  def spotifyme
+    genres= []
+    spotify_user = RSpotify::User.new(@@user_hash)
+    playlists = spotify_user.playlists(limit: 1)
+
+    playlists.each do | playlist |
+      tracks = playlist.tracks
+      tracks.each do | track |
+        artists = track.artists
+        artists.each do | artist |
+          genres += artist.genres
+        end
+      end
+    end
+
+    genres_counted = genres.uniq.map { |x| [x, genres.count(x)] }.sort_by { |k, v| -v }
+
+    @genres_counted_json = genres_counted.to_h
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  get '/auth/spotify/callback', to: 'user#spotify'
+  get '/auth/spotify/callback', to: 'user#spotify_callback'
+  get '/user/spotifyme',        to: 'user#spotifyme'
 
 end
